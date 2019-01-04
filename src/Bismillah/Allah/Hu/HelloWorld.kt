8b64a59a7ce5d8163e6a4412665aa5a903896b94
shehryarahmed <shehreyarahmed5@gmail.com>
2019-01-05 01:50:10 +0500

    working on typealias

--- src/Bismillah/Allah/Hu/HelloWorld.kt
+++ src/Bismillah/Allah/Hu/HelloWorld.kt
@@ -1,5 +1,31 @@
 package Bismillah.Allah.Hu
 
 fun main(args: Array<String>) {
-    println("Hello, world!")
+
+    var number: Int
+    number = 10;
+    number = 20;
+
+    val emplyee : Empolyee;
+
+//    println("Hello, world!")
+    val emp = Empolyee("Ali",12);
+    emp.name = "Ahmed"
+
+    val employee = Empolyee("first",10)
+
+
+    val empolyee2 : Empolyee;
+    val number2 = 100
+
+    if(number < number2){
+        empolyee2 = Empolyee("Jane",10)
+    }else{
+        empolyee2 = Empolyee("KHAN",20)
+
+    }
+}
+
+class Empolyee(var name:String, val age : Int){
+
 }
