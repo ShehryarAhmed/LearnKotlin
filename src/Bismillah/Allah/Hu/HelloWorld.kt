7585405b06ebe4141072f4a5397c53227d8c7d70
shehryarahmed <shehreyarahmed5@gmail.com>
2019-01-06 08:58:59 +0500

    working on compare to objects with equal opreator

--- src/Bismillah/Allah/Hu/HelloWorld.kt
+++ src/Bismillah/Allah/Hu/HelloWorld.kt
@@ -4,31 +4,48 @@ typealias  Empol = Set<Empolyee>
 
 fun main(args: Array<String>) {
 
-    var number: Int
-    number = 10;
-    number = 20;
+//    var number: Int
+//    number = 10;
+//    number = 20;
+//
+//    val emplyee : Empolyee;
+//
+////    println("Hello, world!")
+//    val emp = Empolyee("Ali",12);
+//    emp.name = "Ahmed"
+//
+//    val employee = Empolyee("first",10)
+//
+//    val myEmp : Empol
+//
+//    val empolyee2 : Empolyee;
+//    val number2 = 100
+//
+//    if(number < number2){
+//        empolyee2 = Empolyee("Jane",10)
+//    }else{
+//        empolyee2 = Empolyee("KHAN",20)
+//
+//    }
 
-    val emplyee : Empolyee;
+    var emp1 = Empolyee("Ali",12)
+    val emp2 = Empolyee("Ahmed",12)
+    val emp3 = Empolyee("Ahmed",12)
+    emp1 = emp2
+    println(emp1 === emp2)
+    println(emp2 === emp3)
+    println(emp1 == emp2)
+    println(emp2 == emp3)
 
-//    println("Hello, world!")
-    val emp = Empolyee("Ali",12);
-    emp.name = "Ahmed"
-
-    val employee = Empolyee("first",10)
-
-    val myEmp : Empol
-
-    val empolyee2 : Empolyee;
-    val number2 = 100
-
-    if(number < number2){
-        empolyee2 = Empolyee("Jane",10)
-    }else{
-        empolyee2 = Empolyee("KHAN",20)
-
-    }
 }
 
 class Empolyee(var name:String, val age : Int){
+    override fun equals(obj: Any?): Boolean {
 
+        if(obj is Empolyee){
+            return  name == obj.name && age == obj.age
+        }else{
+            return false
+        }
+    }
 }
